1b40a226011bf9c6dc5a46496379453f405e6641
vercruyssetim <tim.vercruysse@cegeka.be>
2023-05-09 11:44:22 +0200

    added new encapsulation exercise

---
+++ 10-programming-fundamentals/20-oop/10-objects-and-classes/22-encapsulation/src/main/java/basic/codelab02/Money.java
@@ -0,0 +1,34 @@
+package basic.codelab02;
+
+import java.util.Objects;
+
+public class Money {
+    public double value;
+    public String currency;
+
+    public Money(double value, String currency) {
+        this.value = value;
+        this.currency = currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "value=" + value +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return Double.compare(money.value, value) == 0 && Objects.equals(currency, money.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, currency);
+    }
+}
---
+++ 10-programming-fundamentals/20-oop/10-objects-and-classes/22-encapsulation/src/test/java/basic/codelab02/BankAccountTest.java
@@ -0,0 +1,25 @@
+package basic.codelab02;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BankAccountTest {
+
+    @Test
+    void aEuroAccount() {
+        BankAccount bankAccount = new BankAccount(new Money(10.50, "Euro"));
+        bankAccount.addBalance(new Money(9, "Euro"));
+        bankAccount.addBalance(new Money(21, "Dollar"));
+
+        Assertions.assertThat(bankAccount.balance).isEqualTo(new Money(38.61, "Euro"));
+    }
+
+    @Test
+    void aDollarAccount() {
+        BankAccount bankAccount = new BankAccount(new Money(10.50, "Dollar"));
+        bankAccount.addBalance(new Money(9, "Dollar"));
+        bankAccount.addBalance(new Money(21, "Euro"));
+
+        Assertions.assertThat(bankAccount.balance).isEqualTo(new Money(42.60, "Dollar"));
+    }
+}
